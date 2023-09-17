599258c199f17b625791ec51f8f8042aa1fc966d
86 <10976595+chenruiho@user.noreply.gitee.com>
2023-09-17 16:13:33 +0800

    feat: 🎨 完善阿里云oss提交

---
+++ sky-server/src/main/java/com/sky/config/Ossconfigurition.java
@@ -0,0 +1,28 @@
+package com.sky.config;
+
+import com.sky.properties.AliOssProperties;
+import com.sky.utils.AliOssUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Chen
+ * @date 2023/9/17
+ * @apiNote 配置类用于创建阿里oss
+ */
+@Configuration
+@Slf4j
+public class Ossconfigurition {
+
+    @Bean
+    @ConditionalOnMissingBean
+    public AliOssUtil aliOssUtil(AliOssProperties aliOssProperties) {
+        log.info("开始创建阿里云文件上次工具类对象{}", aliOssProperties);
+        return new AliOssUtil(aliOssProperties.getEndpoint(),
+                aliOssProperties.getAccessKeyId(),
+                aliOssProperties.getAccessKeySecret(),
+                aliOssProperties.getBucketName());
+    }
+}
